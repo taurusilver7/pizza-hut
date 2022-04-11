c443f1b67cf23a9fe0889a8f3b2a4ef988fcb6b6
taurusilver7 <prasad20697@gmail.com>
2022-04-11 14:23:40 +0530

    button functions

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-// menu navbar burger button.
-
 let navbar = document.querySelector(".header .flex .navbar");
+let account = document.querySelector(".user-account");
+let myOrders = document.querySelector(".my-orders");
 
+// menu navbar burger button.
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
 };
 
-window.onscroll = () => {
+// login & register button function
+document.querySelector("#user-btn").onclick = () => {
+  account.classList.toggle("active");
+};
+
+// login & register button close functionality.
+document.querySelector("#close-account").onclick = () => {
+  account.classList.remove("active");
+};
+
+// Previous orders list display.
+document.querySelector("#order-btn").onclick = () => {
+  myOrders.classList.toggle("active");
+};
+
+// Order display hidden
+document.querySelector("#close-orders").onclick = () => {
+  myOrders.classList.remove("active");
+};
+
+document.window.onscroll = () => {
   navbar.classList.remove("active");
 };
