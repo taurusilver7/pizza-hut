c2096b5eb8f6720c5c0a5135f95561d94ca45d7c
taurusilver7 <prasad20697@gmail.com>
2022-04-14 22:16:12 +0530

    home-page

--- script.js
+++ script.js
@@ -43,3 +43,31 @@ document.window.onscroll = () => {
   myOrders.classList.remove("active");
   cart.classList.remove("active");
 };
+
+let slides = document.querySelectorAll(
+  ".home-bg #home .slide-container .slide"
+);
+let index = 0;
+
+function next() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+function prev() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
+
+let accordion = document.querySelectorAll(
+  ".faq .accordion-container .accordion"
+);
+
+accordion.forEach((acco) => {
+  acco.onclick = () => {
+    accordion.forEach((remove) => remove.classList.remove("active"));
+    acco.classList.add("active");
+  };
+});
